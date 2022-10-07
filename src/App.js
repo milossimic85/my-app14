9288aa9ae297eb5aa457217ac42ba8d3d23b604f
Milos Simic <zunjinjo@gmail.com>
2022-10-07 21:53:28 +0200

    App modified!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 import { Switch, Route, Redirect } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import Watch from "./components/Watch/Watch";
 
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import AuthContext from "./components/store/auth-context";
+import Search from "./Search/Search";
+import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
+import Location from "./components/Location/Location";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const authCtx = useContext(AuthContext);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [show, setShow] = useState(false);
+  const [value, setValue] = useState(null);
+
+  const onSearchChangeHandler = (searchData) => {
+    const [lat, lon] = searchData.value.split("");
+    const currentWeatherFetch = fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7e0fdbf14c28aac7448823feef24d155&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      })
+      .catch(console.log);
+  };
+  const addSucessHandler = (data) => {
+    if (data) {
+      toast.success(data);
+      setValue(data);
+      setShow(true);
+    }
+  };
+
+  const addHandler = (data) => {
+    if (data) {
+      toast.error(data);
+      setValue(data);
+      setShow(true);
+    }
+  };
   return (
     <Layout>
+      <Watch />
+      {show && (
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      )}
+      <Location />
+      <Search onSearchChange={onSearchChangeHandler} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+
       <Switch>
         <Route path="/" exact>
           <HomePage />
         </Route>
         {!authCtx.isLoggedIn && (
           <Route path="/auth">
-            <AuthPage />
+            <AuthPage onAddDataSuccess={addSucessHandler} onAdd={addHandler} />
           </Route>
         )}
         <Route path="/profile">
